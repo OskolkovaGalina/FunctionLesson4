63756dd67f8090bf072dd58acdc7c2eecb2c647b
galina_oskolkova <galidos@mail.ru>
2023-01-30 16:03:29 +0300

    Задача 27 решена

--- Program.cs
+++ Program.cs
@@ -3,24 +3,67 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
-int numberA = ReadInt("Введите число A: ");
-int numberB = ReadInt("Введите число B: ");
-ToDegree(numberA, numberB);
+//int numberA = ReadInt("Введите число A: ");
+//int numberB = ReadInt("Введите число B: ");
+//ToDegree(numberA, numberB);
 
 // Функция возведения в степень
-void ToDegree(int a, int b)
-{
-    int result = 1;
-    for (int i = 1; i <= b; i++)
-    {
-        result = result * a;
-    }
-    Console.WriteLine(result);
-}
+//void ToDegree(int a, int b)
+//{
+    //int result = 1;
+    //for (int i = 1; i <= b; i++)
+    //{
+        //result = result * a;
+    //}
+    //Console.WriteLine(result);
+//}
+
+// Функция ввода
+//int ReadInt(string message)
+//{
+    //Console.WriteLine(message);
+    //return Convert.ToInt32(Console.ReadLine());
+//}
+
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт 
+//сумму цифр в числе. Через строку решать нельзя.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
 
 // Функция ввода
 int ReadInt(string message)
 {
-    Console.WriteLine(message);
+    Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция подсчета цифр в числе
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
 }
