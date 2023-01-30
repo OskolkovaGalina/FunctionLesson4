7eb1c2b244d8f05b1100acfc48029d07002db047
galina_oskolkova <galidos@mail.ru>
2023-01-30 16:47:45 +0300

    Задача 29 решена

--- Program.cs
+++ Program.cs
@@ -32,9 +32,53 @@
 //82 -> 10
 //9012 -> 12
 
-int number = ReadInt("Введите число: ");
-int len = NumberLen(number);
-SumNumbers(number, len);
+//int number = ReadInt("Введите число: ");
+//int len = NumberLen(number);
+//SumNumbers(number, len);
+
+// Функция ввода
+//int ReadInt(string message)
+//{
+    //Console.Write(message);
+    //return Convert.ToInt32(Console.ReadLine());
+//}
+
+// Функция подсчета цифр в числе
+//int NumberLen(int a)
+//{
+    //int index = 0;
+    //while (a > 0)
+    //{
+        //a /= 10;
+        //index++;
+    //}
+    //return index;
+//}
+
+// Функция вывода суммы цифр в числе
+//void SumNumbers(int n, int len)
+//{
+    //int sum = 0;
+    //for (int i = 1; i <= len; i++)
+    //{
+        //sum += n % 10;
+        //n /= 10;
+    //}
+    //Console.WriteLine(sum);
+//}
+
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 целых чисел 
+//с клавиатуры и далее выводит массив на экран в одну строку
+
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i  <randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(0,9);
+    Console.Write(randomArray[i] + " ");
+}
 
 
 // Функция ввода
@@ -42,28 +86,4 @@ int ReadInt(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
-}
-
-// Функция подсчета цифр в числе
-int NumberLen(int a)
-{
-    int index = 0;
-    while (a > 0)
-    {
-        a /= 10;
-        index++;
-    }
-    return index;
-}
-
-// Функция вывода суммы цифр в числе
-void SumNumbers(int n, int len)
-{
-    int sum = 0;
-    for (int i = 1; i <= len; i++)
-    {
-        sum += n % 10;
-        n /= 10;
-    }
-    Console.WriteLine(sum);
 }
